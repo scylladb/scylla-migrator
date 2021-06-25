6ccca902c689dc9e2c7a8a86cb253be7f91cb63b
wentingwu <wentingwu@microsoft.com>
2021-06-25 16:43:42 -0400

    Remove unused submodules

--- build.sh
+++ build.sh
@@ -4,29 +4,10 @@ set -e
 set -x
 
 #workaround for number exceptions, once new sbt will be used + 2.12 scala below won't be needed
-export TERM=xterm-color 
-
-#git submodule update --init --recursive
+export TERM=xterm-color
 
 TMPDIR="$PWD"/tmpexec
 mkdir -p "$TMPDIR"
 trap "rm -rf $TMPDIR" EXIT
-pushd spark-cassandra-connector
-sbt -Djava.io.tmpdir="$TMPDIR" ++2.11.12 assembly
-popd
-pushd spark-dynamodb
-sbt assembly
-popd
-pushd spark-kinesis
-sbt assembly
-popd
-
-if [ ! -d "./lib" ]; then
-    mkdir lib
-fi
-
-cp ./spark-cassandra-connector/connector/target/scala-2.11/spark-cassandra-connector-assembly-*.jar ./lib
-cp ./spark-dynamodb/target/scala-2.11/spark-dynamodb-assembly-*.jar ./lib
-cp ./spark-kinesis/target/scala-2.11/spark-streaming-kinesis-asl-assembly-*.jar ./lib
 
 sbt -Djava.io.tmpdir="$TMPDIR" assembly
