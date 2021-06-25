029b3e6186d0b5994e0628ebb683b09ccb7a87bb
wentingwu <wentingwu@microsoft.com>
2021-06-25 16:44:31 -0400

    Remove DynamoDB related code

--- src/main/scala/com/scylladb/migrator/Migrator.scala
+++ src/main/scala/com/scylladb/migrator/Migrator.scala
@@ -1,20 +1,13 @@
 package com.scylladb.migrator
 
-import java.nio.charset.StandardCharsets
-import java.nio.file.{ Files, Paths }
-import java.util.concurrent.{ ScheduledThreadPoolExecutor, TimeUnit }
-
-//import com.amazonaws.services.dynamodbv2.streamsadapter.model.RecordAdapter
-import com.datastax.spark.connector.rdd.partitioner.dht.Token
-import com.datastax.spark.connector.writer._
 import com.scylladb.migrator.config._
-import com.scylladb.migrator.readers.Cassandra
-import org.apache.log4j.{ Level, LogManager, Logger }
+import org.apache.log4j.{Level, LogManager, Logger}
 import org.apache.spark.sql._
-import org.apache.spark.streaming.{ Seconds, StreamingContext }
-//import org.apache.spark.streaming.kinesis.{ KinesisInputDStream, SparkAWSCredentials }
-import sun.misc.{ Signal, SignalHandler }
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
+import java.util.concurrent.ScheduledThreadPoolExecutor
 import scala.util.control.NonFatal
 
 object Migrator {
@@ -49,31 +42,22 @@ object Migrator {
             cassandraSource,
             cassandraSource.preserveTimestamps,
             migratorConfig.skipTokenRanges)
-//        case parquetSource: SourceSettings.Parquet =>
-//          readers.Parquet.readDataFrame(spark, parquetSource)
-//        case dynamoSource: SourceSettings.DynamoDB =>
-//          val tableDesc = DynamoUtils
-//            .buildDynamoClient(dynamoSource.endpoint, dynamoSource.credentials, dynamoSource.region)
-//            .describeTable(dynamoSource.table)
-//            .getTable
-//
-//          readers.DynamoDB.readDataFrame(spark, dynamoSource, tableDesc)
       }
 
     log.info("Created source dataframe; resulting schema:")
     sourceDF.dataFrame.printSchema()
 
-//    val tokenRangeAccumulator =
-//      if (!sourceDF.savepointsSupported) None
-//      else {
-//        val tokenRangeAccumulator = TokenRangeAccumulator.empty
-//        spark.sparkContext.register(tokenRangeAccumulator, "Token ranges copied")
-//
-//        addUSR2Handler(migratorConfig, tokenRangeAccumulator)
-//        startSavepointSchedule(scheduler, migratorConfig, tokenRangeAccumulator)
-//
-//        Some(tokenRangeAccumulator)
-//      }
+    //    val tokenRangeAccumulator =
+    //      if (!sourceDF.savepointsSupported) None
+    //      else {
+    //        val tokenRangeAccumulator = TokenRangeAccumulator.empty
+    //        spark.sparkContext.register(tokenRangeAccumulator, "Token ranges copied")
+    //
+    //        addUSR2Handler(migratorConfig, tokenRangeAccumulator)
+    //        startSavepointSchedule(scheduler, migratorConfig, tokenRangeAccumulator)
+    //
+    //        Some(tokenRangeAccumulator)
+    //      }
 
     log.info("Starting write...")
 
@@ -85,58 +69,7 @@ object Migrator {
             migratorConfig.renames,
             sourceDF.dataFrame,
             sourceDF.timestampColumns)
-//            tokenRangeAccumulator)
-//        case target: TargetSettings.DynamoDB =>
-//          val sourceAndDescriptions = migratorConfig.source match {
-//            case source: SourceSettings.DynamoDB =>
-//              if (target.streamChanges) {
-//                log.info(
-//                  "Source is a Dynamo table and change streaming requested; enabling Dynamo Stream")
-//                DynamoUtils.enableDynamoStream(source)
-//              }
-//              val sourceDesc =
-//                DynamoUtils
-//                  .buildDynamoClient(source.endpoint, source.credentials, source.region)
-//                  .describeTable(source.table)
-//                  .getTable
-//
-//              Some(
-//                (
-//                  source,
-//                  sourceDesc,
-//                  DynamoUtils.replicateTableDefinition(
-//                    sourceDesc,
-//                    target
-//                  )
-//                ))
-//
-//            case _ =>
-//              None
-//          }
-//
-//          writers.DynamoDB.writeDataframe(
-//            target,
-//            migratorConfig.renames,
-//            sourceDF.dataFrame,
-//            sourceAndDescriptions.map(_._3))
-//
-//          sourceAndDescriptions.foreach {
-//            case (source, sourceDesc, targetDesc) =>
-//              log.info("Done transferring table snapshot. Starting to transfer changes")
-//
-//              DynamoStreamReplication.createDStream(
-//                spark,
-//                streamingContext,
-//                source,
-//                target,
-//                sourceDF.dataFrame.schema,
-//                sourceDesc,
-//                targetDesc,
-//                migratorConfig.renames)
-//
-//              streamingContext.start()
-//              streamingContext.awaitTermination()
-//          }
+        //            tokenRangeAccumulator)
       }
     } catch {
       case NonFatal(e) => // Catching everything on purpose to try and dump the accumulator state
@@ -144,64 +77,64 @@ object Migrator {
           "Caught error while writing the DataFrame. Will create a savepoint before exiting",
           e)
     } finally {
-//      tokenRangeAccumulator.foreach(dumpAccumulatorState(migratorConfig, _, "final"))
+      //      tokenRangeAccumulator.foreach(dumpAccumulatorState(migratorConfig, _, "final"))
       scheduler.shutdown()
       spark.stop()
     }
   }
 
-  def savepointFilename(path: String): String =
-    s"${path}/savepoint_${System.currentTimeMillis / 1000}.yaml"
-
   def dumpAccumulatorState(config: MigratorConfig,
-//                           accumulator: TokenRangeAccumulator,
+                           //                           accumulator: TokenRangeAccumulator,
                            reason: String): Unit = {
     val filename =
       Paths.get(savepointFilename(config.savepoints.path)).normalize
-//    val rangesToSkip = accumulator.value.get.map(range =>
-//      (range.range.start.asInstanceOf[Token[_]], range.range.end.asInstanceOf[Token[_]]))
+    //    val rangesToSkip = accumulator.value.get.map(range =>
+    //      (range.range.start.asInstanceOf[Token[_]], range.range.end.asInstanceOf[Token[_]]))
 
     val modifiedConfig = config.copy(
       skipTokenRanges = config.skipTokenRanges
-//        ++ rangesToSkip
+      //        ++ rangesToSkip
     )
 
     Files.write(filename, modifiedConfig.render.getBytes(StandardCharsets.UTF_8))
 
-//    log.info(
-//      s"Created a savepoint config at ${filename} due to ${reason}. Ranges added: ${rangesToSkip}")
+    //    log.info(
+    //      s"Created a savepoint config at ${filename} due to ${reason}. Ranges added: ${rangesToSkip}")
   }
 
-//  def startSavepointSchedule(svc: ScheduledThreadPoolExecutor,
-//                             config: MigratorConfig//,
-////                             acc: TokenRangeAccumulator
-//                            ): Unit = {
-//    val runnable = new Runnable {
-//      override def run(): Unit =
-//        try dumpAccumulatorState(config,"schedule")
-//        catch {
-//          case e: Throwable =>
-//            log.error("Could not create the savepoint. This will be retried.", e)
-//        }
-//    }
-//
-//    log.info(
-//      s"Starting savepoint schedule; will write a savepoint every ${config.savepoints.intervalSeconds} seconds")
-//
-//    svc.scheduleAtFixedRate(runnable, 0, config.savepoints.intervalSeconds, TimeUnit.SECONDS)
-//  }
+  def savepointFilename(path: String): String =
+    s"${path}/savepoint_${System.currentTimeMillis / 1000}.yaml"
 
-//  def addUSR2Handler(config: MigratorConfig, acc: TokenRangeAccumulator) = {
-//    log.info(
-//      "Installing SIGINT/TERM/USR2 handler. Send this to dump the current progress to a savepoint.")
-//
-//    val handler = new SignalHandler {
-//      override def handle(signal: Signal): Unit =
-//        dumpAccumulatorState(config, acc, signal.toString)
-//    }
-//
-//    Signal.handle(new Signal("USR2"), handler)
-//    Signal.handle(new Signal("TERM"), handler)
-//    Signal.handle(new Signal("INT"), handler)
-//  }
+  //  def startSavepointSchedule(svc: ScheduledThreadPoolExecutor,
+  //                             config: MigratorConfig//,
+  ////                             acc: TokenRangeAccumulator
+  //                            ): Unit = {
+  //    val runnable = new Runnable {
+  //      override def run(): Unit =
+  //        try dumpAccumulatorState(config,"schedule")
+  //        catch {
+  //          case e: Throwable =>
+  //            log.error("Could not create the savepoint. This will be retried.", e)
+  //        }
+  //    }
+  //
+  //    log.info(
+  //      s"Starting savepoint schedule; will write a savepoint every ${config.savepoints.intervalSeconds} seconds")
+  //
+  //    svc.scheduleAtFixedRate(runnable, 0, config.savepoints.intervalSeconds, TimeUnit.SECONDS)
+  //  }
+
+  //  def addUSR2Handler(config: MigratorConfig, acc: TokenRangeAccumulator) = {
+  //    log.info(
+  //      "Installing SIGINT/TERM/USR2 handler. Send this to dump the current progress to a savepoint.")
+  //
+  //    val handler = new SignalHandler {
+  //      override def handle(signal: Signal): Unit =
+  //        dumpAccumulatorState(config, acc, signal.toString)
+  //    }
+  //
+  //    Signal.handle(new Signal("USR2"), handler)
+  //    Signal.handle(new Signal("TERM"), handler)
+  //    Signal.handle(new Signal("INT"), handler)
+  //  }
 }
--- src/main/scala/com/scylladb/migrator/config/SourceSettings.scala
+++ src/main/scala/com/scylladb/migrator/config/SourceSettings.scala
@@ -1,20 +1,11 @@
 package com.scylladb.migrator.config
 
-import cats.implicits._
 import io.circe.syntax._
-import io.circe.{ Decoder, DecodingFailure, Encoder, Json, ObjectEncoder }
-import io.circe.generic.semiauto.{ deriveDecoder, deriveEncoder }
+import io.circe.{Decoder, DecodingFailure, Encoder, Json}
 
-case class DynamoDBEndpoint(host: String, port: Int) {
-  def renderEndpoint = s"${host}:${port}"
-}
-
-object DynamoDBEndpoint {
-  implicit val encoder = deriveEncoder[DynamoDBEndpoint]
-  implicit val decoder = deriveDecoder[DynamoDBEndpoint]
-}
 
 sealed trait SourceSettings
+
 object SourceSettings {
   case class Cassandra(host: String,
                        port: Int,
@@ -26,17 +17,7 @@ object SourceSettings {
                        fetchSize: Int,
                        preserveTimestamps: Boolean,
                        where: Option[String])
-      extends SourceSettings
-  case class DynamoDB(endpoint: Option[DynamoDBEndpoint],
-                      region: Option[String],
-                      credentials: Option[AWSCredentials],
-                      table: String,
-                      scanSegments: Option[Int],
-                      readThroughput: Option[Int],
-                      throughputReadPercent: Option[Float],
-                      maxMapTasks: Option[Int])
-      extends SourceSettings
-  case class Parquet(path: String, credentials: Option[AWSCredentials]) extends SourceSettings
+    extends SourceSettings
 
   implicit val decoder: Decoder[SourceSettings] = Decoder.instance { cursor =>
     cursor.get[String]("type").flatMap {
@@ -54,12 +35,7 @@ object SourceSettings {
             "preserveTimestamps",
             "where")(Cassandra.apply)
           .apply(cursor)
-//      case "parquet" =>
-//        Decoder
-//          .forProduct2("path", "credentials")(Parquet.apply)
-//          .apply(cursor)
-//      case "dynamo" | "dynamodb" =>
-//        deriveDecoder[DynamoDB].apply(cursor)
+
       case otherwise =>
         Left(DecodingFailure(s"Unknown source type: ${otherwise}", cursor.history))
     }
@@ -81,17 +57,6 @@ object SourceSettings {
           "where")(Cassandra.unapply(_: Cassandra).get)
         .encodeObject(s)
         .add("type", Json.fromString("cassandra"))
-        .asJson
-    case s: DynamoDB =>
-      deriveEncoder[DynamoDB]
-        .encodeObject(s)
-        .add("type", Json.fromString("dynamodb"))
-        .asJson
-    case s: Parquet =>
-      Encoder
-        .forProduct2("path", "credentials")(Parquet.unapply(_: Parquet).get)
-        .encodeObject(s)
-        .add("type", Json.fromString("parquet"))
         .asJson
   }
 }
