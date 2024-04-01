2bf431f7619e1916cc8182edb04c8f425dc5f12d
Julien Richard-Foy <julien@richard-foy.fr>
2024-04-01 15:43:50 +0200

    Check services’ readiness by waiting for a 200 HTTP response instead of just checking that the TCP port is open

--- .github/wait-for-port.sh
+++ .github/wait-for-port.sh
@@ -4,7 +4,7 @@ port=$1
 echo "Waiting for port ${port}"
 attempts=0
 max_attempts=30
-while ! nc -z 127.0.0.1 $port && [[ $attempts < $max_attempts ]] ; do
+while ! curl -s "http://127.0.0.1:$port" > /dev/null && [[ $attempts -lt $max_attempts ]] ; do
     attempts=$((attempts+1))
     sleep 1;
     echo "waiting... (${attempts}/${max_attempts})"
