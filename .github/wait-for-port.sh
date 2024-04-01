f2ed5c19dbfa1af4e88c39dddb3807ea9f12f844
Julien Richard-Foy <julien@richard-foy.fr>
2024-04-01 15:56:32 +0200

    Increase timeout delay

--- .github/wait-for-port.sh
+++ .github/wait-for-port.sh
@@ -3,7 +3,7 @@
 port=$1
 echo "Waiting for port ${port}"
 attempts=0
-max_attempts=30
+max_attempts=60
 while ! curl -s "http://127.0.0.1:$port" > /dev/null ; do
     [[ $attempts -ge $max_attempts ]] && echo "Failed!" && exit 1
     attempts=$((attempts+1))
