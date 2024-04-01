7dde95177435bdafe6ad6286ddb695dee3ee7c52
Julien Richard-Foy <julien@richard-foy.fr>
2024-04-01 15:47:55 +0200

    Fail when we reach the maximum number of attempts

--- .github/wait-for-port.sh
+++ .github/wait-for-port.sh
@@ -4,7 +4,8 @@ port=$1
 echo "Waiting for port ${port}"
 attempts=0
 max_attempts=30
-while ! curl -s "http://127.0.0.1:$port" > /dev/null && [[ $attempts -lt $max_attempts ]] ; do
+while ! curl -s "http://127.0.0.1:$port" > /dev/null ; do
+    [[ $attempts -ge $max_attempts ]] && echo "Failed!" && exit 1
     attempts=$((attempts+1))
     sleep 1;
     echo "waiting... (${attempts}/${max_attempts})"
