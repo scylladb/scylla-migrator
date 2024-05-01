2b7f0762d830d606d953522718f6eac347b29007
Julien Richard-Foy <julien@richard-foy.fr>
2024-05-01 12:45:06 +0200

    Copy files from awslabs/emr-dynamodb-connector
    Content was copied from this point of the history:
    https://github.com/awslabs/emr-dynamodb-connector/tree/9726a0d8dfbb6b3e0a56b7c06fcd78f764b7e39b/emr-dynamodb-hadoop/src/main/java/org/apache/hadoop/dynamodb

---
+++ load-balanced-emr-dynamodb-hadoop/src/main/java/org/apache/hadoop/dynamodb/preader/AbstractRecordReadRequest.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
+ * except in compliance with the License. A copy of the License is located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "LICENSE.TXT" file accompanying this file. This file is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under the License.
+ */
+
+package org.apache.hadoop.dynamodb.preader;
+
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import org.apache.hadoop.dynamodb.DynamoDBConstants;
+import org.apache.hadoop.dynamodb.preader.RateController.RequestLimit;
+
+import java.util.Map;
+
+public abstract class AbstractRecordReadRequest {
+
+  protected final AbstractReadManager readMgr;
+  protected final DynamoDBRecordReaderContext context;
+
+  protected final Map<String, AttributeValue> lastEvaluatedKey;
+  protected final int segment;
+  protected final String tableName;
+
+  public AbstractRecordReadRequest(AbstractReadManager readMgr, DynamoDBRecordReaderContext
+      context, int segment, Map<String, AttributeValue> lastEvaluatedKey) {
+    this.readMgr = readMgr;
+    this.context = context;
+    this.tableName = context.getConf().get(DynamoDBConstants.INPUT_TABLE_NAME);
+    this.segment = segment;
+    this.lastEvaluatedKey = lastEvaluatedKey;
+  }
+
+  public void read(RequestLimit lim) {
+    signalProgress();
+    PageResults<Map<String, AttributeValue>> pageResults = readNextPage(lim);
+    addPageToMultiplexer(pageResults);
+    reportMetrics(lim, pageResults);
+    enqueueNextPageOrCompleteSegment(pageResults);
+  }
+
+  private void reportMetrics(RequestLimit lim,
+      PageResults<Map<String, AttributeValue>> pageResults) {
+    if (!pageResults.isFailed()) {
+      readMgr.report(lim.readCapacityUnits, pageResults.consumedRcu, pageResults.items.size(),
+          pageResults.retries);
+    }
+  }
+
+  private void signalProgress() {
+    context.getReporter().progress();
+  }
+
+  protected abstract AbstractRecordReadRequest buildNextReadRequest(
+      PageResults<Map<String, AttributeValue>> pageResults);
+
+  protected abstract PageResults<Map<String, AttributeValue>> fetchPage(RequestLimit lim);
+
+  private PageResults<Map<String, AttributeValue>> readNextPage(RequestLimit lim) {
+    try {
+      return fetchPage(lim);
+    } catch (Exception e) {
+      return new PageResults<>(e);
+    }
+  }
+
+  private void addPageToMultiplexer(PageResults<Map<String, AttributeValue>> pageResults) {
+    if (!context.getPageResultMultiplexer().addPageResults(pageResults)) {
+      throw new RuntimeException("Interrupted while adding to the page mux. Aborting.");
+    }
+  }
+
+  private void enqueueNextPageOrCompleteSegment(
+      PageResults<Map<String, AttributeValue>> pageResults) {
+    if (pageResults.lastEvaluatedKey != null) {
+      // Schedule the next page read for this segment
+      readMgr.enqueueReadRequestToTail(buildNextReadRequest(pageResults));
+    } else {
+      // Or mark it as complete
+      readMgr.markSegmentComplete(segment);
+    }
+  }
+}
---
+++ load-balanced-emr-dynamodb-hadoop/src/main/java/org/apache/hadoop/dynamodb/read/ReadIopsCalculator.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
+ * except in compliance with the License. A copy of the License is located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "LICENSE.TXT" file accompanying this file. This file is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under the License.
+ */
+
+package org.apache.hadoop.dynamodb.read;
+
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughputDescription;
+import com.amazonaws.services.dynamodbv2.model.TableDescription;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.dynamodb.DynamoDBClient;
+import org.apache.hadoop.dynamodb.DynamoDBConstants;
+import org.apache.hadoop.dynamodb.IopsCalculator;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+
+public class ReadIopsCalculator implements IopsCalculator {
+
+  private static final Log log = LogFactory.getLog(ReadIopsCalculator.class);
+
+  private final DynamoDBClient dynamoDBClient;
+  private final JobClient jobClient;
+  private final JobConf jobConf;
+  private final String tableName;
+
+  private final double throughputPercent;
+  private final int totalSegments;
+  private final int localSegments;
+
+  public ReadIopsCalculator(JobClient jobClient, DynamoDBClient dynamoDBClient, String tableName,
+                            int totalSegments, int localSegments) {
+    this.jobConf = (JobConf) jobClient.getConf();
+    this.jobClient = jobClient;
+
+    this.dynamoDBClient = dynamoDBClient;
+    this.tableName = tableName;
+    this.totalSegments = totalSegments;
+    this.localSegments = localSegments;
+
+    this.throughputPercent = Double.parseDouble(jobConf.get(DynamoDBConstants
+        .THROUGHPUT_READ_PERCENT, DynamoDBConstants.DEFAULT_THROUGHPUT_PERCENTAGE));
+
+    log.info("Table name: " + tableName);
+    log.info("Throughput percent: " + throughputPercent);
+  }
+
+  public long calculateTargetIops() {
+    double configuredThroughput = Math.floor(Double.parseDouble(
+        jobConf.get(DynamoDBConstants.READ_THROUGHPUT,
+            String.valueOf(getThroughput()))) * throughputPercent);
+    long throughputPerTask = Math.max((long) (configuredThroughput / totalSegments
+        * localSegments), 1);
+
+    log.info("Throughput per task for table " + tableName + " : " + throughputPerTask);
+    return throughputPerTask;
+  }
+
+  private double getThroughput() {
+    TableDescription tableDescription = dynamoDBClient.describeTable(tableName);
+    if (tableDescription.getBillingModeSummary() == null
+        || tableDescription.getBillingModeSummary().getBillingMode()
+            .equalsIgnoreCase(DynamoDBConstants.BILLING_MODE_PROVISIONED)) {
+      ProvisionedThroughputDescription provisionedThroughput = tableDescription
+          .getProvisionedThroughput();
+      return provisionedThroughput.getReadCapacityUnits();
+    }
+    return DynamoDBConstants.DEFAULT_CAPACITY_FOR_ON_DEMAND;
+  }
+}
