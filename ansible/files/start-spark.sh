752cd09dfcf04a5e7d2d2eaf79ce22c4419e013d
Patrick Bossman <20567699+pdbossman@users.noreply.github.com>
2024-05-29 14:31:06 -0400

    add convenience scripts to start/stop spark

---
+++ ansible/files/start-spark.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+
+. spark-env
+
+export SPARK_LOCAL_IP=$SPARK_MASTER_HOST
+
+cd $SPARK_HOME/sbin
+
+./start-master.sh
+
+/bin/mkdir /tmp/spark-events
+
+./start-history-server.sh
+
+./start-slave.sh spark://$SPARK_MASTER_HOST:7077 $SLAVESIZE
+
+./start-shuffle-service.sh
