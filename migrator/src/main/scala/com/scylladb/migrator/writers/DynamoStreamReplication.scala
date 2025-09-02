c3bcbda000f53f308e287f65a913d9d532d5defa
Artem <artem@replika.com>
2025-09-02 09:10:36 +0200

    Group operations by type

--- migrator/src/main/scala/com/scylladb/migrator/writers/DynamoStreamReplication.scala
+++ migrator/src/main/scala/com/scylladb/migrator/writers/DynamoStreamReplication.scala
@@ -14,10 +14,10 @@ import org.apache.spark.streaming.kinesis.{
   SparkAWSCredentials
 }
 import com.scylladb.migrator.DynamoUtils
+import org.apache.hadoop.dynamodb.DynamoDBItemWritable
+import org.apache.hadoop.io.Text
 import software.amazon.awssdk.services.dynamodb.model.{
   AttributeValue => AttributeValueV2,
-  DeleteItemRequest,
-  PutItemRequest,
   TableDescription
 }
 
@@ -33,88 +33,107 @@ object DynamoStreamReplication {
   // We have to deal with multiple representation of the data because `spark-kinesis-dynamodb`
   // uses the AWS SDK V1, whereas `emr-dynamodb-hadoop` uses the AWS SDK V2
   private val operationTypeColumn = "_dynamo_op_type"
+  private val sequenceNumberColumn = "_seq_num"
   private val putOperation = new AttributeValueV1().withBOOL(true)
   private val deleteOperation = new AttributeValueV1().withBOOL(false)
+  private val PUT_OPERATION_TYPE = "PUT"
+  private val DELETE_OPERATION_TYPE = "DELETE"
 
-  private[writers] def run(
-    msgs: RDD[Option[util.Map[String, AttributeValueV1]]],
-    target: TargetSettings.DynamoDB,
-    renamesMap: Map[String, String],
-    targetTableDesc: TableDescription)(implicit spark: SparkSession): Unit = {
-    val rdd = msgs.flatMap(_.toSeq)
+  /**
+    * Groups consecutive operations of the same type into batches for efficient processing
+    * while maintaining sequence order.
+    *
+    * @param operations List of operations sorted by sequence number
+    * @return List of (operation_type, batch) tuples in sequence order
+    */
+  private[writers] def groupConsecutiveOperations(
+    operations: Seq[util.Map[String, AttributeValueV1]])
+    : List[(String, List[util.Map[String, AttributeValueV1]])] = {
+    val getOperationType = (item: util.Map[String, AttributeValueV1]) =>
+      if (item.get(operationTypeColumn) == putOperation) PUT_OPERATION_TYPE
+      else DELETE_OPERATION_TYPE
 
-    val putCount = spark.sparkContext.longAccumulator("putCount")
-    val deleteCount = spark.sparkContext.longAccumulator("deleteCount")
-    val keyAttributeNames = targetTableDesc.keySchema.asScala.map(_.attributeName).toSet
-
-    rdd.foreachPartition { partition =>
-      if (partition.nonEmpty) {
-        val client =
-          DynamoUtils.buildDynamoClient(
-            target.endpoint,
-            target.finalCredentials.map(_.toProvider),
-            target.region,
-            Seq.empty
-          )
-        try {
-          partition.foreach { item =>
-            val isPut = item.get(operationTypeColumn) == putOperation
-
-            val itemWithoutOp = item
-              .entrySet()
-              .stream()
-              .filter(e => e.getKey != operationTypeColumn)
-              .collect(
-                Collectors.toMap(
-                  (e: util.Map.Entry[String, AttributeValueV1]) => e.getKey,
-                  (e: util.Map.Entry[String, AttributeValueV1]) =>
-                    AttributeValueUtils.fromV1(e.getValue)
-                )
-              )
-
-            if (isPut) {
-              putCount.add(1)
-              val finalItem = itemWithoutOp.asScala.map {
-                case (key, value) => renamesMap.getOrElse(key, key) -> value
-              }.asJava
-              try {
-                client.putItem(
-                  PutItemRequest.builder().tableName(target.table).item(finalItem).build())
-              } catch {
-                case e: Exception =>
-                  log.error(s"Failed to put item into ${target.table}", e)
-              }
-            } else {
-              deleteCount.add(1)
-              val keyToDelete = itemWithoutOp.asScala
-                .filter { case (key, _) => keyAttributeNames.contains(key) }
-                .map { case (key, value) => renamesMap.getOrElse(key, key) -> value }
-                .asJava
-              try {
-                client.deleteItem(
-                  DeleteItemRequest.builder().tableName(target.table).key(keyToDelete).build())
-              } catch {
-                case e: Exception =>
-                  log.error(s"Failed to delete item from ${target.table}", e)
-              }
-            }
+    operations
+      .foldLeft(List.empty[(String, List[util.Map[String, AttributeValueV1]])]) {
+        case (Nil, item) =>
+          List((getOperationType(item), List(item)))
+        case ((lastType, lastBatch) :: rest, item) =>
+          val currentType = getOperationType(item)
+          if (currentType == lastType) {
+            (lastType, lastBatch :+ item) :: rest
+          } else {
+            (currentType, List(item)) :: (lastType, lastBatch) :: rest
           }
-        } finally {
-          client.close()
+      }
+      .reverse
+  }
+
+  private[writers] def run(msgs: RDD[Option[util.Map[String, AttributeValueV1]]],
+                           target: TargetSettings.DynamoDB,
+                           renamesMap: Map[String, String],
+                           targetTableDesc: TableDescription,
+                           dynamoDB: DynamoDB.type)(implicit spark: SparkSession): Unit =
+    if (!msgs.isEmpty()) {
+      val rdd = msgs
+        .collect { case Some(item) => item: util.Map[String, AttributeValueV1] }
+        .repartition(Runtime.getRuntime.availableProcessors() * 2)
+
+      val allOperationsSorted = rdd.collect().sortBy(_.get(sequenceNumberColumn).getS)
+
+      if (allOperationsSorted.nonEmpty) {
+        val grouped = groupConsecutiveOperations(allOperationsSorted.toIndexedSeq)
+
+        val (totalPuts, totalDeletes) = grouped.foldLeft((0L, 0L)) {
+          case ((putAcc, deleteAcc), (operationType, batch)) =>
+            if (operationType == DELETE_OPERATION_TYPE) {
+              val deletableItems = spark.sparkContext.parallelize(batch).map { item =>
+                item
+                  .entrySet()
+                  .stream()
+                  .filter(e => e.getKey != operationTypeColumn && e.getKey != sequenceNumberColumn)
+                  .collect(
+                    Collectors.toMap(
+                      (e: util.Map.Entry[String, AttributeValueV1]) => e.getKey,
+                      (e: util.Map.Entry[String, AttributeValueV1]) =>
+                        AttributeValueUtils.fromV1(e.getValue)
+                    ))
+              }
+              dynamoDB.deleteRDD(target, targetTableDesc, deletableItems)(spark)
+              (putAcc, deleteAcc + batch.length)
+            } else {
+              val writablePuts = spark.sparkContext.parallelize(batch).map { item =>
+                (
+                  new Text,
+                  new DynamoDBItemWritable(
+                    item
+                      .entrySet()
+                      .stream()
+                      .filter(e =>
+                        e.getKey != operationTypeColumn && e.getKey != sequenceNumberColumn)
+                      .collect(
+                        Collectors.toMap(
+                          (e: util.Map.Entry[String, AttributeValueV1]) => e.getKey,
+                          (e: util.Map.Entry[String, AttributeValueV1]) =>
+                            AttributeValueUtils.fromV1(e.getValue)
+                        ))
+                  ))
+              }
+              dynamoDB.writeRDD(target, renamesMap, writablePuts, targetTableDesc)(spark)
+              (putAcc + batch.length, deleteAcc)
+            }
+        }
+
+        if (totalPuts > 0 || totalDeletes > 0) {
+          log.info(s"""
+                      |Changes applied in sequence order:
+                      |  - ${totalPuts} items UPSERTED
+                      |  - ${totalDeletes} items DELETED
+                      |""".stripMargin)
+        } else {
+          log.info("No changes to apply")
         }
       }
     }
-
-    if (putCount.value > 0 || deleteCount.value > 0) {
-      log.info(s"""
-                  |Changes to be applied:
-                  |  - ${putCount.value} items to UPSERT
-                  |  - ${deleteCount.value} items to DELETE
-                  |""".stripMargin)
-    } else {
-      log.info("No changes to apply")
-    }
-  }
 
   def createDStream(spark: SparkSession,
                     streamingContext: StreamingContext,
@@ -145,6 +164,9 @@ object DynamoStreamReplication {
               case "REMOVE"            => deleteOperation
             }
           newMap.put(operationTypeColumn, operationType)
+          newMap.put(
+            sequenceNumberColumn,
+            new AttributeValueV1().withS(rec.getDynamodb.getSequenceNumber))
           Some(newMap)
 
         case _ => None
@@ -166,7 +188,8 @@ object DynamoStreamReplication {
         msgs.asInstanceOf[RDD[Option[util.Map[String, AttributeValueV1]]]],
         target,
         renamesMap,
-        targetTableDesc)(spark)
+        targetTableDesc,
+        DynamoDB)(spark)
     }
 
 }
