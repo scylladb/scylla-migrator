bb1ad1349c94bda347be22f347e02ba321547975
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2026-05-21 11:00:31 -0400

    Update Scylla Spark connector to 4.1.3

--- migrator/src/main/scala/com/scylladb/migrator/writers/Scylla.scala
+++ migrator/src/main/scala/com/scylladb/migrator/writers/Scylla.scala
@@ -128,7 +128,7 @@ object Scylla {
     *
     * We drop them from the DataFrame before writing so that both sides agree on the column count.
     */
-  // Mirrors the private TableWriter.InternalColumns from spark-scylladb-connector 4.1.2.
+  // Mirrors the private TableWriter.InternalColumns from spark-scylladb-connector 4.1.3.
   // If the connector version changes, verify this set is still in sync.
   private[writers] val InternalColumns: Set[String] = Set("solr_query")
 
